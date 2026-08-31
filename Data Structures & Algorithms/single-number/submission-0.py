1abdb7b74f28aa35c510bfc397e0c081117c2387
Natalie Lim <88850618+natalie-lim@users.noreply.github.com>
2026-08-31 00:57:24 -0400

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        s = set()
+
+        for n in nums:
+            if n not in s:
+                s.add(n)
+            else:
+                s.remove(n)
+        
+        return s.pop()
